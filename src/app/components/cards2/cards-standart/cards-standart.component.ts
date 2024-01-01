c6d0c14e8dc3b6b0d8e7a52da107d9c633dab83f
Nariman <n.murzakaev@big3.ru>
2024-01-01 19:22:02 +0300

    feat(cards-standart): add link

--- src/app/components/cards2/cards-standart/cards-standart.component.ts
+++ src/app/components/cards2/cards-standart/cards-standart.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Card } from 'src/app/components/cards/cards.component';
 import { PriceComponent } from 'src/app/components/price';
+import { LinkComponent } from '../../link';
 
 @Component({
   selector: 'app-cards-standart',
@@ -9,6 +10,7 @@ import { PriceComponent } from 'src/app/components/price';
   imports: [
     CommonModule,
     PriceComponent,
+    LinkComponent,
   ],
   templateUrl: './cards-standart.component.html',
   styleUrls: ['./cards-standart.component.scss'],
@@ -17,4 +19,5 @@ import { PriceComponent } from 'src/app/components/price';
 export class CardsStandartComponent {
   @Input() items: Card[] = [];
   @Input() title!: string;
+  @Input() linkText!: string;
 }
