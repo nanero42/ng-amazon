201ca689b22271c75057b2ad7bafaf70f4d219cb
Nariman <n.murzakaev@big3.ru>
2024-01-02 15:15:10 +0300

    refactor(home-page): rename home to home-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('src/app/pages').then(m => m.HomeComponent),
+    loadComponent: () => import('src/app/pages').then(m => m.HomePageComponent),
   },
 ];
