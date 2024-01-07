3ff69a6e179301cacb0c6e3be6cfaca4944a04fa
Nariman <n.murzakaev@big3.ru>
2024-01-07 16:13:50 +0300

    refactor(component): replace inline code with components in cards-standard component

--- src/app/components/cards2/cards-standard/cards-standard.component.ts
+++ src/app/components/cards2/cards-standard/cards-standard.component.ts
@@ -2,6 +2,9 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Card } from '../../cards/cards.component';
 import { PriceComponent } from '../../price';
+import { LinkComponent } from '../../link';
+import { ViewedCountComponent } from '../../viewed-count';
+import { ProductTitleComponent } from '../../product-title';
 
 @Component({
   selector: 'app-cards-standard',
@@ -9,6 +12,9 @@ import { PriceComponent } from '../../price';
   imports: [
     CommonModule,
     PriceComponent,
+    LinkComponent,
+    ViewedCountComponent,
+    ProductTitleComponent,
   ],
   templateUrl: './cards-standard.component.html',
   styleUrls: ['./cards-standard.component.scss'],
@@ -17,4 +23,7 @@ import { PriceComponent } from '../../price';
 export class CardsStandardComponent {
   @Input() title!: string;
   @Input() items: Card[] = [];
+  @Input() showViewedCount = false;
+  @Input() showPrice = false;
+  @Input() linkText!: string;
 }
