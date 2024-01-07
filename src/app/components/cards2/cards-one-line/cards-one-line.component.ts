b999978c1634d70a709c4ab66dde29a7258ba1e8
Nariman <n.murzakaev@big3.ru>
2024-01-07 16:10:38 +0300

    refactor(component): add product-title component to cards-one-line component

--- src/app/components/cards2/cards-one-line/cards-one-line.component.ts
+++ src/app/components/cards2/cards-one-line/cards-one-line.component.ts
@@ -1,11 +1,15 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Card } from '../../cards/cards.component';
+import { ProductTitleComponent } from '../../product-title';
 
 @Component({
   selector: 'app-cards-one-line',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ProductTitleComponent,
+  ],
   templateUrl: './cards-one-line.component.html',
   styleUrls: ['./cards-one-line.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
